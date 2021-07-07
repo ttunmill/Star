b4948446388d8e515949794a29fc2d0a15dcdd5d
ttunmill <ttunmill@gmail.com>
2021-07-07 18:58:30 +0900

    star

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// var now_img;
+// var last_img;
+
+// window.onload = function() {
+//     setInterval("fn_change(), 3000");
+// }
+
+// function fn_change() {
+//     now_img = $("img").eq(0);
+//     last_img = $("img").eq(-1);
+
+//     last_img.addClass("img_1").css("opacity", 0).animate({"opacity":1},1000 ,function() {
+//         $(".bg_img").prepend(last_img);
+//         now_img.removeClass("img_1");
+//     });
+// }
+
