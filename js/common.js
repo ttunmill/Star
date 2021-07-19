dead2e7b741d77dfb875f4dc54d50c017f89dae2
ttunmill <ttunmill@gmail.com>
2021-07-19 18:52:27 +0900

    project

--- js/common.js
+++ js/common.js
@@ -1,9 +1,7 @@
 $("#hamburger").click(function() {
 	$("#sidenav").css("right", "0");
-$("body").addClass("dark")
 });
 $("#close-sidenav").click(function() {
 	$("#sidenav").css("right", "-200px");
-$("body").removeClass("dark");
 });
 
